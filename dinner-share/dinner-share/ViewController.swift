e4861d89dd66e78c0c8949e95429ac8b104533c9
Christopher Wang <wang967@usc.edu>
2018-02-03 13:15:46 -0800

    initial commit

---
+++ dinner-share/dinner-share/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dinner-share
+//
+//  Created by Chris Wang on 2/3/18.
+//  Copyright © 2018 Chris Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
