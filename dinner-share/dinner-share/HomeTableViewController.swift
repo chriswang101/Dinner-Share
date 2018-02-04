28ef0ff5c7134efce202113f0ed3d4a1298bd4ba
Christopher Wang <wang967@usc.edu>
2018-02-04 01:36:00 -0800

    main screen working

--- dinner-share/dinner-share/HomeTableViewController.swift
+++ dinner-share/dinner-share/HomeTableViewController.swift
@@ -17,6 +17,11 @@ class HomeTableViewController: UITableViewController {
     lazy var nearYouVCs = [UIViewController]()
     lazy var topRatedVCs = [UIViewController]()
     
+    let formatter = DateFormatter()
+    
+    
+    var frame: CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     let recommendationMeals = [
         Meal(imageName: "bread.jpg", date: Date(timeIntervalSinceNow: 10000), description: "Simple meals and pleasures", distance: 2.11),
         Meal(imageName: "burger.jpg", date: Date(timeIntervalSinceNow: 20000), description: "Healthy American dining", distance: 2.39),
@@ -34,23 +39,46 @@ class HomeTableViewController: UITableViewController {
         Meal(imageName: "pizza.jpg", date: Date(timeIntervalSinceNow: 90000), description: "PIZZA!", distance: 1.3)
     ]
     
-    func populatePageVCs(VCs: [UIViewController], meals: [Meal]) {
+    func createPageVCs(meals: [Meal]) -> [UIViewController] {
+        var VCs = [UIViewController]()
         for meal in meals {
-            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier) as! PageContentViewController
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "pageContentViewController") as! PageContentViewController
             //vc.delegate = self
             
             vc.loadView()
             
-            vc.imageView = UIImage
-            vc.descriptionLabel.text =
+            vc.imageView.image = UIImage(named: meal.imageName)
+            vc.descriptionLabel.text = meal.description
+            vc.dateLabel.text = formatter.string(from: meal.date)
+            vc.distanceLabel.text = String(format: "%.2f", meal.distance)
             
-            vc.location = CLLocation(latitude: latitude, longitude: longitude)
-            pageViewVCs.append(vc)
+            VCs.append(vc)
         }
+        return VCs
+    }
+    
+    func populate(scrollView: UIScrollView, with VCs: [UIViewController]) {
+        var frame: CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
+        
+        for index in 0 ..< VCs.count {
+            frame.origin.x = CGFloat(index) * scrollView.frame.size.width
+            frame.size = CGSize(width: scrollView.frame.size.width * 0.97, height: scrollView.frame.size.height)
+            scrollView.isPagingEnabled = true
+            
+            let contentView: UIView! = VCs[index].view
+            contentView.frame = frame
+            contentView.center = CGPoint(x: CGFloat(index) * scrollView.frame.size.width + scrollView.frame.size.width * CGFloat(0.5), y: scrollView.frame.size.height / 2)
+            
+            scrollView.addSubview(contentView)
+        }
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(VCs.count), height: scrollView.frame.size.height)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Configure date formatter
+        formatter.dateFormat = "MM-dd-yy HH:mm"
         
         // Configure tableView appearance
         self.tableView.separatorStyle = .none
@@ -63,6 +91,14 @@ class HomeTableViewController: UITableViewController {
         
         topRatedScrollView.delegate = self
         topRatedScrollView.clipsToBounds = false
+        
+        // Do VC filling
+        recommendationVCs = createPageVCs(meals: recommendationMeals)
+        nearYouVCs = createPageVCs(meals: nearYouMeals)
+        topRatedVCs = createPageVCs(meals: topRatedMeals)
+        populate(scrollView: recommendationsScrollView, with: recommendationVCs)
+        populate(scrollView: nearYouScrollView, with: nearYouVCs)
+        populate(scrollView: topRatedScrollView, with: topRatedVCs)
     }
 
     override func didReceiveMemoryWarning() {
@@ -83,6 +119,4 @@ class HomeTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 0
     }
-
-
 }
